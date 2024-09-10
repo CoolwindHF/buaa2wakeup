4bc7cfe88e449caf270982badecd7b4c533bd0b1
CoolwindHF <765614306@qq.com>
2024-09-10 15:28:38 +0800

    ver-1 支持生成ics文件，可以导入系统日历

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ python3 main.py
 然后根据提示输入内容即可获得`schedule.csv`
 
 保存并导入wakeup即可
+
+## 导入日历
+
+支持生成ics文件，让你彻底不用给~~wakeup掏钱~~
+
+生成ics文件导入日历的方法可以参考[该链接](https://jackyu.cn/tech/apple-ics-import/)
--- main.py
+++ main.py
@@ -212,10 +212,10 @@ def convert_schedule_to_csv(schedule_list):
             weeks = find[0][1].split(",")
             week_list = []
             for week in weeks:
-                if "单" in week:
+                if re.findall(r"单", week):
                     week = week[:-4]
                     week += "单"
-                elif "双" in week:
+                elif re.findall(r"双", week):
                     week = week[:-4]
                     week += "双"
                 else:
@@ -235,6 +235,99 @@ def convert_schedule_to_csv(schedule_list):
         writer = csv.writer(f)
         writer.writerows(list_for_csv)
 
+    return list_for_csv
+
+daily_time = {
+    1: ["000000", "004500"],
+    2: ["005000", "013500"],
+    3: ["015000", "023500"],
+    4: ["024000", "032500"],
+    5: ["033000", "041500"],
+    6: ["060000", "064500"],
+    7: ["065000", "073500"],
+    8: ["075000", "083500"],
+    9: ["084000", "092500"],
+    10: ["093000", "101500"],
+    11: ["110000", "114500"],
+    12: ["115000", "123500"],
+    13: ["124000", "132500"],
+    14: ["133000", "141500"],
+}
+
+from datetime import datetime, timedelta
+import uuid
+first_day_of_term = datetime(2024,9,10)
+def set_the_first_day_of_term(year, month, day):
+    global first_day_of_term
+    input_day = datetime(year, month, day)
+    if input_day.weekday() != 0:
+        print("输入的日期不是周一，请重新输入！")
+        return False
+    first_day_of_term = datetime(year, month, day)
+    return True
+
+def set_the_default_first_day_of_term():
+    global first_day_of_term
+    today = datetime.today()
+    today_weekday = today.weekday()
+    if today_weekday != 0:
+        first_day_of_term = today - timedelta(days=today_weekday)
+    else:
+        first_day_of_term = today
+
+def convert_schedule_to_icaleander(list_for_csv):
+    with open("schedule.ics", "w") as f:
+        f.write("BEGIN:VCALENDAR\n")
+        f.write("VERSION:2.0\n")
+        f.write("PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n")
+        f.write("CALSCALE:GREGORIAN\n")
+        for each_class in list_for_csv:
+            name = each_class[0]
+            day = each_class[1]
+            start_section = each_class[2]
+            end_section = each_class[3]
+            teacher = each_class[4]
+            place = each_class[5]
+            weeks = each_class[6].split("、")
+            for week in weeks:
+                f.write("BEGIN:VEVENT\n")
+                f.write("UID:buaa2ical-{}-CoolwindHF\n".format(str(uuid.uuid4())))
+                # print(week)
+                week = week.split("-")
+                every_2_weeks = False
+                if len(week) == 1:
+                    start_week = int(week[0])
+                    end_week = int(week[0])
+                else:
+                    if re.findall(r"单|双", week[1]):
+                        every_2_weeks = True
+                        start_week = int(week[0])
+                        end_week = int(week[1][:-1])
+                    else:
+                        start_week = int(week[0])
+                        end_week = int(week[1])
+                start_day = (first_day_of_term + timedelta(days=(int(day) - 1) + (int(start_week) - 1) * 7)).strftime("%Y%m%d")
+                end_day = (first_day_of_term + timedelta(days=(int(day) - 1) + (int(end_week) - 1) * 7)).strftime("%Y%m%d")
+                DTSTART = start_day + "T" + daily_time[int(start_section)][0] + "Z"
+                DTEND = start_day + "T" + daily_time[int(end_section)][1] + "Z"
+                f.write("DTSTART:{}\n".format(DTSTART))
+                f.write("DTEND:{}\n".format(DTEND))
+                if len(week) == 1:
+                    description = f"第{start_week}周\\n第{start_section} - {end_section}节\\n{place}\\n{teacher}\\n"
+                else:
+                    if not every_2_weeks:
+                        description = f"第{start_week} - {end_week}周\\n第{start_section} - {end_section}节\\n{place}\\n{teacher}\\n"
+                    else:
+                        description = f"第{start_week} - {end_week}{week[1][-1:]}周\\n第{start_section} - {end_section}节\\n{place}\\n{teacher}\\n"
+                f.write(f"DESCRIPTION:{description}\n")
+                f.write(f"SUMMARY:{name}\n")
+                f.write(f"LOCATION:{place} {teacher}\n")
+                if not every_2_weeks:
+                    f.write(f"RRULE:FREQ=WEEKLY;INTERVAL=1;UNTIL={end_day}\n")
+                else:
+                    f.write(f"RRULE:FREQ=WEEKLY;INTERVAL=2;UNTIL={end_day}\n")
+                f.write("END:VEVENT\n")
+
 if __name__ == "__main__":
     id = input("请输入统一认证学号：").strip()
     passwd = input("请输入统一认证密码：").strip()
@@ -242,6 +335,19 @@ if __name__ == "__main__":
     print("账号{}登陆成功！".format(id))
     year = input("请输入学年，如“2024-2025-1”代表2024-2025学年第一学期：").strip()
     schedule_list = get_schedule(gs_session, _WEU, year)
-    convert_schedule_to_csv(schedule_list)
-    print("课程表已保存为schedule.csv文件！请发送到手机后倒入wakeup课程表！")
-   
+    # print(schedule_list)
+    list_for_csv = convert_schedule_to_csv(schedule_list)
+    print("课程表已保存为schedule.csv文件！请发送到手机后导入wakeup课程表！")
+    to_ical = input("是否将课程表转换为iCalendar文件？(y/n):").strip()
+    if to_ical == "y":
+        while True:
+            first_day = input("请输入学期第一周的周一日期，如“2024-09-02”，直接回车跳过默认以当前周作为学期第一周：").strip().split("-")
+            if len(first_day) != 3:
+                set_the_default_first_day_of_term()
+            else:
+                year, month, day = first_day[0], first_day[1], first_day[2]
+                if not set_the_first_day_of_term(int(year), int(month), int(day)):
+                    continue
+            break
+        convert_schedule_to_icaleander(list_for_csv)
+        print("课程表已保存为schedule.ics文件！请导入日历软件！")
