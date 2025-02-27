b09f85e39c19915dbe5c15992b60e3162114c46e
hjzts <119279415+hjzts@users.noreply.github.com>
2025-02-27 17:25:42 +0800

    fix🐛:/ 修改获取GS_SESSIONID错误的bug

--- main.py
+++ main.py
@@ -1,68 +1,44 @@
+import csv
+import uuid
+from datetime import datetime, timedelta
+import re
 import requests
 from bs4 import BeautifulSoup
 from requests.packages import urllib3
 
 urllib3.disable_warnings()
 
+
 def login(id, passwd):
-    
+
     def get_execution():
         cookies = {
-            '_7da9a': 'http://10.0.3.60:8080',
+            '_7da9a': 'http://10.0.3.35:8080',
         }
 
         headers = {
             'Host': 'sso.buaa.edu.cn',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.100 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Sec-Fetch-Site': 'same-site',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"macOS"',
-            'Accept-Language': 'zh-CN',
-            'Referer': 'https://byxt.buaa.edu.cn/',
-            'Priority': 'u=0, i',
-            'Connection': 'keep-alive',
         }
 
         params = {
             'service': 'https://byxt.buaa.edu.cn/jwapp/sys/homeapp/index.do',
         }
 
-        response = requests.get('https://sso.buaa.edu.cn/login', params=params, cookies=cookies, headers=headers, verify=False, allow_redirects=False)
+        response = requests.get('https://sso.buaa.edu.cn/login', params=params,
+                                cookies=cookies, headers=headers)
+        # print(response.text)
         soup = BeautifulSoup(response.text, 'html.parser')
         execution = soup.find('input', {'name': 'execution'}).get('value')
         # print(execution)
         return execution
-    
+
     def get_location(id, passwd, execution):
         cookies = {
-            '_7da9a': 'http://10.0.3.60:8080',
+            '_7da9a': 'http://10.0.3.35:8080',
         }
 
         headers = {
             'Host': 'sso.buaa.edu.cn',
-            'Cache-Control': 'max-age=0',
-            'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"macOS"',
-            'Accept-Language': 'zh-CN',
-            'Upgrade-Insecure-Requests': '1',
-            'Origin': 'https://sso.buaa.edu.cn',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.100 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Referer': 'https://sso.buaa.edu.cn/login?service=https%3A%2F%2Fbyxt.buaa.edu.cn%2Fjwapp%2Fsys%2Fhomeapp%2Findex.do',
-            'Priority': 'u=0, i',
-            'Connection': 'keep-alive',
         }
 
         data = {
@@ -74,102 +50,77 @@ def login(id, passwd):
             '_eventId': 'submit',
         }
 
-        response = requests.post('https://sso.buaa.edu.cn/login', cookies=cookies, headers=headers, data=data, verify=False, allow_redirects=False)
+        response = requests.post('https://sso.buaa.edu.cn/login', cookies=cookies,
+                                 headers=headers, data=data, allow_redirects=False)
         # print(response.headers)
-        # CASTGC = response.headers.get('Set-Cookie').split(';')[0].split('=')[-1]
-        location = response.headers.get("Location").strip().split("=")[-1]
+        CASTGC = response.headers.get(
+            'Set-Cookie').split(';')[0].split('=')[-1]
+        location = response.headers.get(
+            "Location").strip().split('=')[-1]  # ticket
         # print(CASTGC)
+        # print(location)
         return location
 
     def get_gs_sessionid(location):
         headers = {
             'Host': 'byxt.buaa.edu.cn',
-            'Cache-Control': 'max-age=0',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.100 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Sec-Fetch-Site': 'same-site',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"macOS"',
-            'Accept-Language': 'zh-CN',
-            'Referer': 'https://sso.buaa.edu.cn/',
-            # 'Accept-Encoding': 'gzip, deflate, br',
-            'Priority': 'u=0, i',
-            'Connection': 'keep-alive',
         }
 
         params = {
             "ticket": location,
         }
 
-        response = requests.get('https://byxt.buaa.edu.cn/jwapp/sys/homeapp/index.do', params=params, headers=headers, verify=False, allow_redirects=False)
+        response = requests.get('https://byxt.buaa.edu.cn/jwapp/sys/homeapp/index.do',
+                                params=params, headers=headers, allow_redirects=False)
 
         # print(response.headers)
-        gs_sessionid = response.headers.get("Set-Cookie").strip().split(";")[0].split("=")[-1]
-        return gs_sessionid
-    
+        import re
+        pattern = r'GS_SESSIONID=([0-9a-f]{32});'
+        match = re.search(pattern, response.headers.get("Set-Cookie").strip())
+        if match:
+            gs_sessionid = match.group(1)
+            print(gs_sessionid)
+            return gs_sessionid
+        else:
+            print("GS_SESSIONID not found!")
+
     def get_WEU(gs_sessionid):
+        # print(gs_sessionid)
         cookies = {
             'GS_SESSIONID': gs_sessionid,
         }
 
         headers = {
             'Host': 'byxt.buaa.edu.cn',
-            'Cache-Control': 'max-age=0',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.100 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Sec-Fetch-Site': 'same-site',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"macOS"',
-            'Accept-Language': 'zh-CN',
-            'Referer': 'https://sso.buaa.edu.cn/',
-            'Priority': 'u=0, i',
-            'Connection': 'keep-alive',
         }
 
-        response = requests.get('https://byxt.buaa.edu.cn/jwapp/sys/homeapp/index.do', cookies=cookies, headers=headers, verify=False, allow_redirects=False)
-        _WEU = response.headers.get("Set-Cookie").strip().split(",")[-1].split(";")[0].split("=")[-1]
+        response = requests.get('https://byxt.buaa.edu.cn/jwapp/sys/homeapp/index.do',
+                                cookies=cookies, headers=headers, allow_redirects=True)
+        # print(response.headers)
+        _WEU = response.headers.get(
+            "Set-Cookie").strip().split(",")[-1].split(";")[0].split("=")[-1]
+        # print(_WEU)
         return _WEU
 
     execution = get_execution()
     location = get_location(id, passwd, execution)
     gs_sessionid = get_gs_sessionid(location)
     _WEU = get_WEU(gs_sessionid)
+    # print("execution: ", execution)
+    # print("location: ", location)
+    # print("gs_sessionid: ", gs_sessionid)
+    # print("_WEU: ", _WEU)
     return gs_sessionid, _WEU
+
 
 def get_schedule(gs_sessionid, _WEU, term):
     cookies = {
         'GS_SESSIONID': gs_sessionid,
         '_WEU': _WEU,
-        }
+    }
 
     headers = {
         'Host': 'byxt.buaa.edu.cn',
-        'Cache-Control': 'max-age=0',
-        'Sec-Ch-Ua': '"Chromium";v="127", "Not)A;Brand";v="99"',
-        'Accept-Language': 'zh-CN',
-        'Sec-Ch-Ua-Mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.100 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Accept': 'application/json',
-        'Fetch-Api': 'true',
-        'Sec-Ch-Ua-Platform': '"macOS"',
-        'Origin': 'https://byxt.buaa.edu.cn',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://byxt.buaa.edu.cn/jwapp/sys/homeapp/home/index.html?av=1725888244394&contextPath=/jwapp',
-        'Priority': 'u=1, i',
-        'Connection': 'keep-alive',
     }
 
     data = {
@@ -185,13 +136,14 @@ def get_schedule(gs_sessionid, _WEU, term):
         data=data,
         verify=False,
     )
-    
+    # print(response.text)
+    # print(response.content.decode('utf-8'))
+
     schedule_json = response.json()
     schedule_list = schedule_json["datas"]["arrangedList"]
     return schedule_list
 
-import csv
-import re
+
 def convert_schedule_to_csv(schedule_list):
     with open("schedule.csv", "w") as f:
         writer = csv.writer(f)
@@ -230,12 +182,13 @@ def convert_schedule_to_csv(schedule_list):
             append_list.append(place)
             append_list.append(week_all)
             list_for_csv.append(append_list)
-    
+
     with open("schedule.csv", "a") as f:
         writer = csv.writer(f)
         writer.writerows(list_for_csv)
 
     return list_for_csv
+
 
 daily_time = {
     1: ["000000", "004500"],
@@ -254,9 +207,9 @@ daily_time = {
     14: ["133000", "141500"],
 }
 
-from datetime import datetime, timedelta
-import uuid
-first_day_of_term = datetime(2024,9,10)
+first_day_of_term = datetime(2024, 9, 10)
+
+
 def set_the_first_day_of_term(year, month, day):
     global first_day_of_term
     input_day = datetime(year, month, day)
@@ -266,6 +219,7 @@ def set_the_first_day_of_term(year, month, day):
     first_day_of_term = datetime(year, month, day)
     return True
 
+
 def set_the_default_first_day_of_term():
     global first_day_of_term
     today = datetime.today()
@@ -274,6 +228,7 @@ def set_the_default_first_day_of_term():
         first_day_of_term = today - timedelta(days=today_weekday)
     else:
         first_day_of_term = today
+
 
 def convert_schedule_to_icaleander(list_for_csv):
     with open("schedule.ics", "w") as f:
@@ -306,9 +261,12 @@ def convert_schedule_to_icaleander(list_for_csv):
                     else:
                         start_week = int(week[0])
                         end_week = int(week[1])
-                start_day = (first_day_of_term + timedelta(days=(int(day) - 1) + (int(start_week) - 1) * 7)).strftime("%Y%m%d")
-                end_day = (first_day_of_term + timedelta(days=(int(day) - 1) + (int(end_week) - 1) * 7)).strftime("%Y%m%d")
-                DTSTART = start_day + "T" + daily_time[int(start_section)][0] + "Z"
+                start_day = (first_day_of_term + timedelta(days=(int(day) -
+                             1) + (int(start_week) - 1) * 7)).strftime("%Y%m%d")
+                end_day = (first_day_of_term + timedelta(days=(int(day) -
+                           1) + (int(end_week) - 1) * 7)).strftime("%Y%m%d")
+                DTSTART = start_day + "T" + \
+                    daily_time[int(start_section)][0] + "Z"
                 DTEND = start_day + "T" + daily_time[int(end_section)][1] + "Z"
                 f.write("DTSTART:{}\n".format(DTSTART))
                 f.write("DTEND:{}\n".format(DTEND))
@@ -329,6 +287,7 @@ def convert_schedule_to_icaleander(list_for_csv):
                 f.write("END:VEVENT\n")
         f.write("END:VCALENDAR\n")
 
+
 if __name__ == "__main__":
     id = input("请输入统一认证学号：").strip()
     passwd = input("请输入统一认证密码：").strip()
@@ -342,7 +301,8 @@ if __name__ == "__main__":
     to_ical = input("是否将课程表转换为iCalendar文件？(y/n):").strip()
     if to_ical == "y":
         while True:
-            first_day = input("请输入学期第一周的周一日期，如“2024-09-02”，直接回车跳过默认以当前周作为学期第一周：").strip().split("-")
+            first_day = input(
+                "请输入学期第一周的周一日期，如“2024-09-02”，直接回车跳过默认以当前周作为学期第一周：").strip().split("-")
             if len(first_day) != 3:
                 set_the_default_first_day_of_term()
             else:
